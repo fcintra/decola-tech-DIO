7d93286a5631957322016c38301ef67faea54fb1
fcintra <matheus.cintra_8@hotmail.com>
2022-05-25 16:43:23 -0300

    applying addEventListener

--- contador/assets/scripts.js
+++ contador/assets/scripts.js
@@ -3,45 +3,21 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
 
-var btnSubtrair = document.getElementById('subtrair');
-var btnAcrescentar = document.getElementById('acrescentar');
-
-var verificaCurrentNumber = parseInt(currentNumberWrapper.textContent)
+    
+    btnSubtrair.addEventListener('click', decrement)
+    btnAcrescentar.addEventListener('click', increment)
 
 
 //função para acrescentar ao contador
 function increment(){
-    let verificaCurrentNumber = parseInt(currentNumberWrapper.textContent)
-    console.log(verificaCurrentNumber)
-    if(verificaCurrentNumber >= 10 - 1){
-        btnAcrescentar.disabled = true;
-    }else{
-        btnAcrescentar.disabled = false;
-    }
-
     currentNumber = currentNumber + 1;
-
     currentNumberWrapper.innerHTML = currentNumber;
 }
 
 
 //função para subtrair no contador
 function decrement(){
-    let verificaCurrentNumber = parseInt(currentNumberWrapper.textContent)
-
-    console.log(verificaCurrentNumber)
-    if(verificaCurrentNumber <= 1){
-        console.log('menor que 0')
-        btnSubtrair.disabled = true;
-    }
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
 }
-    if(verificaCurrentNumber >= 0){
-        btnSubtrair.disabled = false
-        btnSubtrair.addEventListener('click', decrement)
-
-    }
-    btnAcrescentar.addEventListener('click', increment)
-
-console.log(typeof verificaCurrentNumber)
+    
